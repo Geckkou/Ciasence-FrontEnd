e9f5d17e24b7a2fcb5ba7a0ff0b8cc6a53bbe985
Geckkou <alexandre.asr2001@hotmail.com>
2021-10-15 16:50:21 -0300

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { FormularioClienteComponent } from './formulario/formulario-cliente/form
 import { FormularioProjetoProdutoComponent } from './formulario/formulario-projeto-produto/formulario-projeto-produto.component';
 import { PaginationComponent } from './pagination/pagination.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,10 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/formulario/formulario-cliente/formulario-cliente.component.ts
+++ src/app/formulario/formulario-cliente/formulario-cliente.component.ts
@@ -11,5 +11,4 @@ export class FormularioClienteComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
