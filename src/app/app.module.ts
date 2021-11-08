16e6fd0e16ea3f9c921ab2351d620b715ee06d8e
Geckkou <alexandre.asr2001@hotmail.com>
2021-11-08 19:16:49 -0300

    melhorando o display dos cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormularioComponent } from './formulario/formulario.component';
 import { MenuComponent } from './views/menu/menu.component';
 import { TablesComponent } from './tables/tables.component';
-import { FormularioProjetoComponent } from './formulario/formulario-projeto/formulario-projeto.component';
-import { FormularioClienteComponent } from './formulario/formulario-cliente/formulario-cliente.component';
-import { FormularioProjetoProdutoComponent } from './formulario/formulario-projeto-produto/formulario-projeto-produto.component';
 import { PaginationComponent } from './pagination/pagination.component';
 import { HomeComponent } from './views/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -32,12 +28,8 @@ import { DialogProdutoProjetoComponent } from './views/dialog-produto-projeto/di
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent,
     MenuComponent,
     TablesComponent,
-    FormularioProjetoComponent,
-    FormularioClienteComponent,
-    FormularioProjetoProdutoComponent,
     PaginationComponent,
     HomeComponent,
     DialogClientesComponent,
